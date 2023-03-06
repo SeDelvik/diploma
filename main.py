0c4f13178c85e8550f46234cde057db6bb4b3fad
adubrovina <adubrovina@at-consulting.ru>
2023-03-06 17:05:16 +0300

    Мелкие изменения

--- main.py
+++ main.py
@@ -4,19 +4,6 @@ from gui import start_gui
 
 
 def main():
-    # Chromosome.create_task_list('./res/testTaskList.json')
-    # chrom = Chromosome()
-    # for tsk in chrom.task_list:
-    #     print(tsk)
-    #
-    # chrom2 = Chromosome()
-    # print(f"self: {chrom}")
-    # print(f"parent2: {chrom2}")
-    #
-    # for obj in chrom.one_point_crossingover(chrom2, 2):
-    #     print(f"children: {obj}")
-    #
-    # SimpleGeneticAlgorithm(10, './res/testTaskList.json', [0, 0, 0, 0])
     start_gui()
 
 
